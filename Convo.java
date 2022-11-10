f7b1ede070b318c14c688fdf15920a4cefdd9377
Minh Phan <minhsg.phan@gmail.com>
2022-11-09 22:49:28 -0600

    remove unused methods

--- Convo.java
+++ Convo.java
@@ -134,45 +134,4 @@ public class Convo
     {
         
     }
-
-    public void dialogBox()
-    {
-        /*textFont(fontDialog);
-        pushMatrix();
-        translate(playerX-195, playerY+80, 0.9);
-
-        beginShape(QUADS);
-        texture(choicebox); 
-        vertex(0,0,  0,0);
-        vertex(390,0,  1,0);
-        vertex(390,60,  1,1);
-        vertex(0,60,  0,1);
-        endShape();*/
-    }
-
-    // methods for navigating the script files
-
-    // getResponseLine takes the response given by the player (1, 2, or 3) as a char, the current line (% after the choice),
-    // and the script we are using, and returns the line we need to jump to for the response
-    /*private int getResponseLine(char response, int line, String[] script)
-    {
-        .while true
-        {
-            if(script[line].charAt(0) == response)
-                return line + 1;
-            line++;
-        }
-    }*/
-
-    // choices takes the current line (% before choice) and the script we are using, and returns the choices as one string to print out
-    /*private String choices(int line, String[] script)
-    {
-        String toReturn = "";
-        line++;
-        while (script[line] != '%')
-        {
-            toReturn += script[line] + "\n"
-        }
-        return toReturn;
-    }*/
 }
